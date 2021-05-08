a50a5a7a4330c334d41d9d9e8ebb86935424ff42
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-05-08 11:04:24 +0900

    [Update]: configure Project

---
+++ InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 白数叡司 on 2021/05/08.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
