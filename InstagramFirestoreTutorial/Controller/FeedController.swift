3a5e1bbfdf4a0566bcaa0ea4b2ccb7cd930e6a17
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-05-10 00:28:16 +0900

    [Update]: configure Feed Cell UI

--- InstagramFirestoreTutorial/Controller/FeedController.swift
+++ InstagramFirestoreTutorial/Controller/FeedController.swift
@@ -23,7 +23,7 @@ class FeedController: UICollectionViewController {
     
     func configureUI() {
         collectionView.backgroundColor = .white
-        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.register(FeedCell.self, forCellWithReuseIdentifier: reuseIdentifier)
     }
 }
 
@@ -32,12 +32,11 @@ class FeedController: UICollectionViewController {
 
 extension FeedController {
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
-        cell.backgroundColor = .red
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FeedCell
         return cell
     }
 }
---
+++ InstagramFirestoreTutorial/View/FeedCell.swift
@@ -0,0 +1,62 @@
+//
+//  FeedCell.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 白数叡司 on 2021/05/10.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    // MARK: - Properties
+    
+    private let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.isUserInteractionEnabled = true
+        imageView.image = #imageLiteral(resourceName: "venom-7")
+        return imageView
+    }()
+    
+    private lazy var usernameButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitleColor(.black, for: .normal)
+        button.setTitle("venom", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+        button.addTarget(self, action: #selector(didTapUsername), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .white
+        
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor,
+               left: leftAnchor,
+               paddingTop: 12,
+               paddingLeft: 12)
+        profileImageView.setDimensions(height: 40, width: 40)
+        profileImageView.layer.cornerRadius = 40 / 2
+        
+        addSubview(usernameButton)
+        usernameButton.centerY(inView: profileImageView,
+                               leftAnchor: profileImageView.rightAnchor,
+                               paddingLeft: 8)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Action
+    
+    @objc func didTapUsername() {
+        print("DEBUG: did tap username...")
+    }
+}
