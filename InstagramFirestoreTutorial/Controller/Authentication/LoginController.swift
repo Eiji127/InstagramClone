e40c0d3f3a38c197fd500c9cd0436b67b025c16c
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-05-11 22:13:41 +0900

    [Update]: configure Login Page UI

---
+++ InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
@@ -0,0 +1,134 @@
+//
+//  LoginController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 白数叡司 on 2021/05/11.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let iconImage: UIImageView = {
+        let imageView = UIImageView(image: #imageLiteral(resourceName: "Instagram_logo_white"))
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let emailTextField: UITextField = {
+       let textField = UITextField()
+        textField.borderStyle = .none
+        textField.textColor = .white
+        textField.keyboardAppearance = .dark
+        textField.keyboardType = .emailAddress
+        textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
+        textField.layer.cornerRadius = 50 / 3
+        textField.setHeight(50)
+        textField.attributedPlaceholder = NSAttributedString(string: "Email",
+                                                             attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+       let textField = UITextField()
+        textField.borderStyle = .none
+        textField.textColor = .white
+        textField.keyboardAppearance = .dark
+        textField.keyboardType = .emailAddress
+        textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
+        textField.layer.cornerRadius = 50 / 3
+        textField.setHeight(50)
+        textField.attributedPlaceholder = NSAttributedString(string: "Password",
+                                                             attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        textField.isSecureTextEntry = true
+        return textField
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Log In", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.3647058904, green: 0.06666667014, blue: 0.9686274529, alpha: 1)
+        button.layer.cornerRadius = 50 / 2
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.setHeight(50)
+        return button
+    }()
+    
+    private let forgetPasswordButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let attrString: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7),
+                                                               .font: UIFont.systemFont(ofSize: 16)]
+        let attributedTitle = NSMutableAttributedString(string: "Forget you password? ",
+                                                        attributes: attrString)
+        let boldAttrString: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7),
+                                                                   .font: UIFont.boldSystemFont(ofSize: 16)]
+        attributedTitle.append(NSAttributedString(string: "Get help signing in.",
+                                                  attributes: boldAttrString))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+    
+    private let dontHaveAccountButton: UIButton = {
+        let button = UIButton(type: .system)
+        
+        let attrString: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7),
+                                                               .font: UIFont.systemFont(ofSize: 16)]
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account? ",
+                                                        attributes: attrString)
+        let boldAttrString: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7),
+                                                                   .font: UIFont.boldSystemFont(ofSize: 16)]
+        attributedTitle.append(NSAttributedString(string: "Sign Up",
+                                                  attributes: boldAttrString))
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+    }
+    
+    // MARK: - Helpers
+    
+    func configureUI() {
+        view.backgroundColor = .white
+        navigationController?.navigationBar.isHidden = true
+        navigationController?.navigationBar.barStyle = .black
+        
+        let gradient = CAGradientLayer()
+        gradient.colors = [UIColor.systemPurple.cgColor, UIColor.systemBlue.cgColor]
+        gradient.locations = [0, 1]
+        view.layer.addSublayer(gradient)
+        gradient.frame = view.frame
+        
+        view.addSubview(iconImage)
+        iconImage.centerX(inView: view)
+        iconImage.setDimensions(height: 80, width: 120)
+        iconImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, paddingTop: 32)
+        
+        let stack = UIStackView(arrangedSubviews: [emailTextField,
+                                                   passwordTextField,
+                                                   loginButton,
+                                                   forgetPasswordButton])
+        stack.axis = .vertical
+        stack.spacing = 20
+        
+        view.addSubview(stack)
+        stack.anchor(top: iconImage.bottomAnchor,
+                     left: view.leftAnchor,
+                     right: view.rightAnchor,
+                     paddingTop: 32,
+                     paddingLeft: 32,
+                     paddingRight: 32)
+        
+        view.addSubview(dontHaveAccountButton)
+        dontHaveAccountButton.centerX(inView: view)
+        dontHaveAccountButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor)
+    }
+}
---
+++ InstagramFirestoreTutorial/Controller/Authentication/RegisterController.swift
@@ -0,0 +1,18 @@
+//
+//  RegisterController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 白数叡司 on 2021/05/11.
+//
+
+import UIKit
+
+class RegisterController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
