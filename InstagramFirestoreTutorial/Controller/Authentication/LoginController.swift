d34dcaa4e5e7e0a8411202ea48b16ab14ed7a644
Eiji <64912886+Eiji127@users.noreply.github.com>
2021-05-11 22:31:59 +0900

    [Update]: create custom subclasses for reusablity

--- InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
+++ InstagramFirestoreTutorial/Controller/Authentication/LoginController.swift
@@ -18,30 +18,13 @@ class LoginController: UIViewController {
     }()
     
     private let emailTextField: UITextField = {
-       let textField = UITextField()
-        textField.borderStyle = .none
-        textField.textColor = .white
-        textField.keyboardAppearance = .dark
+        let textField = CustomTextField(placeholder: "Email")
         textField.keyboardType = .emailAddress
-        textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
-        textField.layer.cornerRadius = 50 / 3
-        textField.setHeight(50)
-        textField.attributedPlaceholder = NSAttributedString(string: "Email",
-                                                             attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
         return textField
     }()
     
     private let passwordTextField: UITextField = {
-       let textField = UITextField()
-        textField.borderStyle = .none
-        textField.textColor = .white
-        textField.keyboardAppearance = .dark
-        textField.keyboardType = .emailAddress
-        textField.backgroundColor = UIColor(white: 1, alpha: 0.1)
-        textField.layer.cornerRadius = 50 / 3
-        textField.setHeight(50)
-        textField.attributedPlaceholder = NSAttributedString(string: "Password",
-                                                             attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+        let textField = CustomTextField(placeholder: "Password")
         textField.isSecureTextEntry = true
         return textField
     }()
@@ -59,31 +42,13 @@ class LoginController: UIViewController {
     
     private let forgetPasswordButton: UIButton = {
         let button = UIButton(type: .system)
-        
-        let attrString: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7),
-                                                               .font: UIFont.systemFont(ofSize: 16)]
-        let attributedTitle = NSMutableAttributedString(string: "Forget you password? ",
-                                                        attributes: attrString)
-        let boldAttrString: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7),
-                                                                   .font: UIFont.boldSystemFont(ofSize: 16)]
-        attributedTitle.append(NSAttributedString(string: "Get help signing in.",
-                                                  attributes: boldAttrString))
-        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.attributedTitle(firstPart: "Forget your password? ", secondPart: "Get help siging in.")
         return button
     }()
     
     private let dontHaveAccountButton: UIButton = {
         let button = UIButton(type: .system)
-        
-        let attrString: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7),
-                                                               .font: UIFont.systemFont(ofSize: 16)]
-        let attributedTitle = NSMutableAttributedString(string: "Don't have an account? ",
-                                                        attributes: attrString)
-        let boldAttrString: [NSAttributedString.Key: Any] = [.foregroundColor: UIColor(white: 1, alpha: 0.7),
-                                                                   .font: UIFont.boldSystemFont(ofSize: 16)]
-        attributedTitle.append(NSAttributedString(string: "Sign Up",
-                                                  attributes: boldAttrString))
-        button.setAttributedTitle(attributedTitle, for: .normal)
+        button.attributedTitle(firstPart: "Don't have an account? ", secondPart: "Sign up")
         return button
     }()
     
